d28ebd1d6bd97b16496019b1152e290736140914
thrq <thoriqmuh123.alkapro@gmail.com>
2023-09-10 21:57:29 +0700

    Connect and Create Slider Firebase - 21.55 - Minggu 10 Sep 2023

--- app/src/main/java/com/thrq/biteboxadmin/fragment/SliderFragment.kt
+++ app/src/main/java/com/thrq/biteboxadmin/fragment/SliderFragment.kt
@@ -1,60 +1,105 @@
 package com.thrq.biteboxadmin.fragment
 
+import android.app.Activity
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.thrq.biteboxadmin.R
+import com.thrq.biteboxadmin.databinding.FragmentSliderBinding
+import java.util.UUID
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SliderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SliderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding : FragmentSliderBinding
+    private var imageUrl : Uri? = null
+    private lateinit var dialog : Dialog
+
+
+    private var launchGalleryActivity = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode == Activity.RESULT_OK){
+            imageUrl = it.data!!.data
+            binding.ivSlider.setImageURI(imageUrl)
         }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slider, container, false)
+        binding = FragmentSliderBinding.inflate(layoutInflater)
+
+        dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.progress_layout)
+        dialog.setCancelable(false)
+
+        binding.apply {
+            ivSlider.setOnClickListener{
+                val intent = Intent("android.intent.action.GET_CONTENT")
+                intent.type = "image/*"
+                launchGalleryActivity.launch(intent)
+            }
+
+            btnUpload.setOnClickListener{
+                if (imageUrl != null){
+                  uploadImage(imageUrl!!)
+                }else{
+                    Toast.makeText(requireContext(), "Silahkan pilih gambar", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SliderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SliderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun uploadImage(uri: Uri){
+        dialog.show()
+
+        val fileName = UUID.randomUUID().toString()+".jpg"
+
+        val refStorage = FirebaseStorage.getInstance().reference.child("slider/$fileName")
+        refStorage.putFile(uri)
+            .addOnSuccessListener {
+                it.storage.downloadUrl.addOnSuccessListener {image ->
+                    storeData(image.toString())
                 }
+            }
+            .addOnFailureListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "Terjadi Kesalahan storage", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+    private fun storeData(image: String) {
+
+        val db = Firebase.firestore
+
+        val data = hashMapOf<String, Any>(
+            "img" to image
+        )
+
+        db.collection("slider").document("item").set(data)
+            .addOnSuccessListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "Slider Updated", Toast.LENGTH_SHORT).show()
+            }
+
+            .addOnFailureListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "Terjadi Kesalahan", Toast.LENGTH_SHORT).show()
+
             }
     }
 }
